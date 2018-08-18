ddac98c30dbd51eb141ec68ceb2601dc0c05326e
unselchr <cdojrf@gmail.com>
2018-08-18 04:50:35 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql=require("mysql");
 var connection=mysql.createConnection({
-    host:"mysql://b3596842a7975d:3816f549@us-cdbr-iron-east-01.cleardb.net/heroku_4afa5abc3a63fa0?reconnect=true",
+    host:"us-cdbr-iron-east-01.cleardb.net",
     port:3306,
-    user:"root",
-    password:"Cu3043598",
-    database:"burgers"
+    user:"b3596842a7975d",
+    password:"3816f549",
+    database:"heroku_4afa5abc3a63fa0"
 });
 connection.connect(function(err){
     if(err) throw err;
